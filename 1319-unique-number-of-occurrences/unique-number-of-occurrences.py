1b7e24c5405a06c09e8dcf3500d75a0f25af0ab2
fromhelianthus <165984445+fromhelianthus@users.noreply.github.com>
2024-08-08 22:24:48 +0900

    Time: 31 ms (97.58%) | Memory: 16.6 MB (50.62%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -2,18 +2,14 @@ from collections import Counter
 
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        # 리스트의 요소 별 빈도가 같다면 False, 다르다면 True
-        # 단 하나라도 같다면 False인가?
-
         occurrences = Counter(arr)
-
-        # 중복 검사용 집합
-        seen = set()
-
-        # occurrences의 요소를 반복하면서 
-        for occurrence in occurrences.values():
-            if occurrence in seen:  # 하나라도 일치한다면 false 리턴
+        
+        # 빈도 리스트를 정렬
+        frequencies = sorted(occurrences.values())
+        
+        # 인접한 요소끼리 비교하여 중복 확인
+        for i in range(1, len(frequencies)):
+            if frequencies[i] == frequencies[i - 1]:
                 return False
-            seen.add(occurrence)
         
         return True
