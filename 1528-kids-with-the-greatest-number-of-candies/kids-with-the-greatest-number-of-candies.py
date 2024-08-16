625a542bb3d2e500f0b23aced83a7d2ee07ebee6
June <165984445+fromhelianthus@users.noreply.github.com>
2024-08-17 00:03:37 +0900

    Time: 27 ms (99.18%) | Memory: 16.4 MB (94.94%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,16 +1,14 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         
-        # 각 아이가 가진 캔디와 여분의 캔디를 더한 합이, 이전 캔디의 합보다 적다면 false, 많다면 true.
+        # 각 아이가 가진 캔디와 여분의 캔디를 더한 합이, 아이들이 가진 캔디의 수 중 최대값이 된다면 true, 아니라면 false.
        
-        # 현재 가장 많은 캔디를 가진 아이의 수를 찾습니다.
+        # 1명의 아이가 갖고 있는 가장 많은 캔디의 수
         maxCandies = max(candies)
-        print(maxCandies)
         
+        # 반환할 리스트
         result = []
-        
-        # 각 아이에 대해 extraCandies를 더했을 때,
-        # 그 아이의 캔디 수가 maxCandies보다 크거나 같은지 확인합니다.
+
         for candy in candies:
             if candy + extraCandies >= maxCandies:
                 result.append(True)
