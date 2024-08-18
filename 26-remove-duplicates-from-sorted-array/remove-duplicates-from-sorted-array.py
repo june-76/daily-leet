951be437f7d719b68eac6dd39c2a9888bb5b195f
June <165984445+fromhelianthus@users.noreply.github.com>
2024-08-19 03:07:57 +0900

    Time: 64 ms (97.01%) | Memory: 17.9 MB (75.82%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,5 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        # 해당 코드의 시간복잡도는 O(n^2)
+        # .pop()을 할 필요가 없음. 고유 요소의 개수만 파악하면 됨.
+        # -> 인덱스(k)를 이용하기
         e = len(nums)
         d = 0
 
@@ -12,3 +16,14 @@ class Solution:
                 i += 1
 
         return e - d
+        """
+        
+        # 인덱스 이용
+        k = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k]:
+                k += 1
+                nums[k] = nums[i]
+
+        return k + 1
