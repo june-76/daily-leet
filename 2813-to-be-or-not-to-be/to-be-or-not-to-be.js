3682b04bfdcbe2d830f10a35c12efb0c8b3b8e45
June <fromhelianthus@gmail.com>
2025-01-22 23:17:58 +0900

    Time: 37 ms (99.26%) | Memory: 49 MB (31.25%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expectedVal) {
+            if (val === expectedVal) {
+                return true;
+            } else {
+                // throw "Not Error";
+                // 에러 객체
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(expectedVal) {
+            if (val !== expectedVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
