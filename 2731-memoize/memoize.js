9be87e2470c7e79e9178922d94fe0d9a005ac6d9
June <fromhelianthus@gmail.com>
2025-01-26 00:38:43 +0900

    Time: 303 ms (44.24%) | Memory: 90.7 MB (19.09%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    
+    // let memory = new Map();
+    const memory = new Map();
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+
+        if (memory.has(key)) {
+            return memory.get(key);
+        }
+
+        const result = fn(...args);
+        memory.set(key, result);
+
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
