a6506eb3c3cd558b08c22827b1336b2969df20f5
June <fromhelianthus@gmail.com>
2025-01-26 00:57:58 +0900

    Time: 62 ms (61.35%) | Memory: 50.3 MB (7.36%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,9 +4,12 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    // promise가 완료될 때까지 기다림
+    // promise가 완료(resolve) 될 때까지 대기
     const result1 = await promise1;
     const result2 = await promise2;
+
+    // Promise.all 병렬 처리 가능, 하나라도 실패하면 모두 실패
+    // const resolvedPromiseArray = await Promise.all([promise1, promise2]);
 
     return result1 + result2;
 };
