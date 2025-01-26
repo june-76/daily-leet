9b473aab1584009bf05d00b2576251e547f52fe6
June <fromhelianthus@gmail.com>
2025-01-26 23:08:40 +0900

    Time: 60 ms (80.89%) | Memory: 49.8 MB (34.34%) - LeetSync

---
+++ 2821-timeout-cancellation/timeout-cancellation.js
@@ -0,0 +1,49 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // 캔슬 여부를 판단할 변수(플래그)
+    let cancelled = false;
+
+    // 캔슬을 true로 설정
+    const cancelFn = () => {
+        cancelled = true;
+    };
+
+    // 함수 지연 실행
+    setTimeout(() => {
+        if (!cancelled) {
+            fn(...args);
+        }
+    }, t);
+
+    // cancelFn() 함수 실행
+    return cancelFn;
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
